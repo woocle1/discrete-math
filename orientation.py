4dbc020974762f2f57d00028ad088fbbfdd0bf77
Andrii Tiuska <102793623+woocle1@users.noreply.github.com>
2022-12-14 02:13:21 +0200

    Delete orientation.py

--- orientation.py
+++
@@ -1,6 +0,0 @@
-def graphOrientation(matrix):
-    for x in range(len(matrix)):
-        for y in range(len(matrix)):
-            if y > x:
-                if not matrix[x][y] == matrix[y][x]: "Граф орієнтований"
-    return "Граф може бути як неорієнтованим, так і орієнтованим"
